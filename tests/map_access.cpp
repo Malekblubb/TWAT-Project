a4e5ec37b9728810de1d66bce37271426652344c
Christoph Malek <malekchristoph@gmail.com>
2013-09-13 23:33:59 +0200

    added tests

---
+++ tests/map_access.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2013 Christoph Malek
+ * See LICENSE for more information.
+ */
+
+#include <iostream>
+
+#include <core/tools/tw/datafiles/map.h>
+#include <core/tw_stripped/mapitems.h>
+
+using namespace std;
+using namespace TWAT::TwTools;
+
+
+int main()
+{
+	CTwMap twMap;
+	twMap.Load("/home/hax/.teeworlds/maps/test.map");
+
+
+	if(twMap.IsValid())
+	{
+		cout << "Author: " << twMap.Info().Author() << endl;
+		cout << "Version: " << twMap.Info().Version() << endl;
+		cout << "Credits: " << twMap.Info().Credits() << endl;
+		cout << "License: " << twMap.Info().License() << endl << endl;
+
+
+		for(int g = 0; g < twMap.NumGroups(); ++g)
+		{
+			CTwMapGroup *tmpGroup = twMap.Group(g);
+
+			cout << "[Group " << g << "]" << endl;
+
+			for(int l = 0; l < tmpGroup->NumLayers(); ++l)
+			{
+				CTwMapLayer *tmpLayer = tmpGroup->Layer<CTwMapLayer>(l);
+
+				if(tmpLayer->Type() == LAYERTYPE_TILES)
+				{
+					CTwMapLayerTiles *tmpTilesLayer = tmpGroup->Layer<CTwMapLayerTiles>(l);
+
+					cout << "	[Tileslayer " << l << " (" << tmpTilesLayer->Height() << "x" << tmpTilesLayer->Width() << ")]" << endl;
+
+					if(tmpTilesLayer->HasImage())
+					{
+						CTwMapImage *tmpImage = tmpTilesLayer->Image();
+
+						cout << "		[Image \"" << tmpImage->Name() << "\" (" << tmpImage->Height() << "x" << tmpImage->Width() << ")]" << endl;
+					}
+				}
+
+				if(tmpLayer->Type() == LAYERTYPE_QUADS)
+				{
+					CTwMapLayerQuads *tmpQuadsLayer = tmpGroup->Layer<CTwMapLayerQuads>(l);
+
+					cout << "	[Quadslayer " << l << "]" << endl;
+
+					if(tmpQuadsLayer->HasImage())
+					{
+						CTwMapImage *tmpImage = tmpQuadsLayer->Image();
+
+						cout << "		[Image \"" << tmpImage->Name() << "\" (" << tmpImage->Height() << "x" << tmpImage->Width() << ")]" << endl;
+					}
+				}
+			}
+		}
+	}
+}
--- twat_project.pro
+++ twat_project.pro
@@ -27,3 +27,18 @@ HEADERS +=  $$system("find ./src -name '*.h'") \
 FORMS   +=  $$system("find ./src -name '*.ui'")
 RESOURCES += data/ressources.qrc
 }
+
+test_map_access{
+SOURCES +=  tests/map_access.cpp \
+            src/base/system.cpp \
+            src/core/shared/io_file.cpp \
+            src/core/shared/datafile.cpp \
+            src/core/tools/tw/datafiles/map.cpp \
+            src/core/tools/tw/datafiles/map_datafile.cpp \
+
+HEADERS +=  src/base/system.h \
+            src/core/shared/io_file.h \
+            src/core/shared/datafile.h \
+            src/core/tools/tw/datafiles/map.h \
+            src/core/tools/tw/datafiles/map_datafile.h \
+}
